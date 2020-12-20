fa620e7251355c4047da5b1de9dfd0042c5e81b7
greatandrew <49309440+greatandrew@users.noreply.github.com>
2020-12-20 11:46:05 +0800

    Update and rename 乘法逆元.cpp to ModualrMultipilcativeInverse.cpp

--- ModualrMultipilcativeInverse.cpp
+++ ModualrMultipilcativeInverse.cpp
@@ -1,3 +1,4 @@
+//print the inverse from 1 to n (mod P)
 #include <algorithm>
 #include <iostream>
 #include <cstring>
@@ -7,19 +8,18 @@
 using namespace std;
 typedef long long ll;
 const int N = 3000005;
-int n, lqy;
+int n, P;
 int inv[N];
 
 int main () {
 
-    scanf("%d%d", &n, &lqy);
+    scanf("%d%d", &n, &P);
     inv[0] = 0; inv[1] = 1;
     for(int i = 2; i <= n; ++ i) {
-        ll wyz = inv[lqy % i];
-		inv[i] = lqy - wyz * (lqy / i) % lqy;
+        ll tmp = inv[P % i];
+		inv[i] = P - tmp * (P / i) % P;
     }
     for(int i = 1; i <= n; ++ i) printf("%d\n", inv[i]);
 
 	return 0;
 }
-
